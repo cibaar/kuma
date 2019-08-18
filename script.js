059ac01d6950981bbcf0dbb403a8ac3cfa2ec093
cibaar <cibaar@users.github.com>
2019-08-18 12:18:39 +0300

    hi this is massege

---
+++ script.js
@@ -0,0 +1,21 @@
+var color1 = document.querySelector('.color1');
+var color2 = document.querySelector('.color2');
+var h3 = document.querySelector('.h3');
+var body = document.getElementById('body');
+
+function setgradient(){
+
+    body.style.background = 'linear-gradient(to right,'+ color1.value + "," + color2.value + ')'
+
+
+    h3.textContent = 'linear-gradient(to right,' + color1.value + ',' + color2.value + ')';
+}
+
+
+
+
+
+color1.addEventListener('input', setgradient);
+
+
+color2.addEventListener('input', setgradient)
